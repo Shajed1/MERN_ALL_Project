6a57900657981986b6701e890f173cec5c9dc563
Shajed1 <shajedshohan11@gmail.com>
2026-06-20 01:49:42 +0600

    This is The ecommerce project start from today 20/6/2026

---
+++ Ecomerce-Project/app.js
@@ -0,0 +1,51 @@
+//import all dependency
+const express = require('express');
+const cors = require('cors');
+const router=require('./src/routes/api')
+const mongoose = require('mongoose');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const helmet = require('helmet');
+const path = require('path');
+const app = new express();
+
+
+
+
+app.use(cors());
+
+//security
+app.use(helmet());
+app.use(hpp());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
+app.set('etag',false)
+let limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 3000
+});
+
+app.use(limiter);
+
+//mongodb connection
+let URL = "mongodb+srv://shajed1:shajed1@cluster0.onjdyfa.mongodb.net/MyEccomerce?appName=Cluster0";
+mongoose.connect(URL)
+    .then(()=>{
+        console.log("Connected to MongoDB");
+    })
+    .catch((err)=>{
+        console.log(err);
+    });
+app.use('/api',router);
+
+app.use(express.static('client/dist'));
+
+//
+
+
+//
+app.get("*",function (req,res){
+    res.sendFile(path.resolve(__dirname,'client','dist','index.html'));
+});
+
+module.exports = app;
---
+++ Ecomerce-Project/index.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app=require('./app');
+app.listen(3030,function(){
+    console.log('Server started on port 3030');
+})
